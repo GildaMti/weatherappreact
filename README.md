083e2fb8fcd38fcd00eb9d5ce07c948cde549387
GildaMti <89417592+GildaMti@users.noreply.github.com>
2021-12-05 23:17:16 +0330

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# weatherappreact
+# weather-react1
+ 
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Weather from "./Weather";
+import "./styles.css";
+export default function App(){
+    return (<div className="App">
+        <div className="container">
+        <Weather  defaultCity="New York"/>
+        <footer>
+        This project was coded by Mozhdeh Matalebi and is {" "} <a href="https://github.com" target="_blank">open-sourced on Github </a>
+        </footer>
+    </div>
+    </div>
+);
+}
+
+
+  
